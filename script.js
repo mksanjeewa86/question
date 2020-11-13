4ba3841fffcf7db62134645e8b44e096ed50d9ca
mksanjeewa86 <56330220+mksanjeewa86@users.noreply.github.com>
2020-11-13 23:53:45 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,111 @@
-var texts = 
-[
-    "休みの日は何をして過ごしてる？",
-    "今ハマっていること、おすすめなことは何？",
-    "最近感動したことや涙したことは何？",
-    "学生の頃にやってしまった失敗談",
-    "須田さんの第一印象は？",
-    "会社でどんな仕事をしているの？",
-    "学校ではどんな勉強をしていたの？",
-    "明日地球が滅びるとしたら何がしたい？",
-    "今までの人生で会った人の中で「一番魅力的な人」は？",
-    "学生時代のアルバイトとか部活は？",
-    "宝くじで1億円があたったら何に使う？",
-    "火事のときに持って逃げるもの、3つ教えて！",
-    "取得したい資格",
-    "休みの日の過ごし方",
-    "これはとくに好き、っていう本や映画は？",
-    "外国人になるならどこの国？",
-    "仕事で困ってること",
-    "最近、笑ったことは？",
-    "地元の特産品は？",
-    "１ヶ月毎日食べるとしたら何？",
-    "ひとりで生きていける？",
-    "もしも異性に２４時間なれたら何する？",
-    "最近欲しい家電は",
-    "高校生の時に流行っていた曲"
-];
+var url_string = window.location.href;
+    var url = new URL(url_string);
+    var action = url.searchParams.get("action");
+if(action == 1){
+    var head = ["内定者向け", "新卒向け", "内定者向け", "社員向け", "内定者向け", "社員向け", "内定者向け", "新卒向け", "内定者向け", "社員向け", "内定者向け", "社員向け"];
+    var class_bg = ["info", "success", "info", "primary", "info", "primary", "info", "success", "info", "primary", "info", "primary"];
+    var class_btn = ["primary", "info", "primary", "success", "primary", "success", "primary", "info", "primary", "success", "primary", "success"];
+    var texts = [
+        "休みの日は何をして過ごしてる？",
+        "今ハマっていること、おすすめなことは何？",
+        "学生の頃にやってしまった失敗談",
+        "最近感動したことや涙したことは何？",
+        "学校ではどんな勉強をしていたの？",
+        "会社でどんな仕事をしているの？",
+        "取得したい資格",
+        "須田さんの第一印象は？",
+        "外国人になるならどこの国？",
+        "今までの人生で会った人の中で「一番魅力的な人」は？",
+        "地元の特産品は？",
+        "最近、笑ったことは？"
+    ];
+}else if(action == 2){
+    var head = ["内定者向け", "新卒向け", "内定者向け", "社員向け", "内定者向け", "社員向け", "内定者向け", "新卒向け", "内定者向け", "社員向け", "内定者向け", "社員向け"];
+    var class_bg = ["info", "success", "info", "primary", "info", "primary", "info", "success", "info", "primary", "info", "primary"];
+    var class_btn = ["primary", "info", "primary", "success", "primary", "success", "primary", "info", "primary", "success", "primary", "success"];
+    var texts = [
+        "もしも異性に２４時間なれたら何する？",
+        "今ハマっていること、おすすめなことは何？",
+        "学生の頃にやってしまった失敗談",
+        "最近感動したことや涙したことは何？",
+        "学校ではどんな勉強をしていたの？",
+        "会社でどんな仕事をしているの？",
+        "取得したい資格",
+        "須田さんの第一印象は？",
+        "外国人になるならどこの国？",
+        "今までの人生で会った人の中で「一番魅力的な人」は？",
+        "地元の特産品は？",
+        "最近、笑ったことは？"
+    ];
+}else if(action == 3){
+    var head = ["内定者向け", "社員向け", "社員向け", "内定者向け", "社員向け", "社員向け", "内定者向け", "社員向け", "社員向け", "内定者向け", "社員向け", "社員向け"];
+    var class_bg = ["info", "primary", "primary", "info", "primary", "primary", "info", "primary", "primary", "info", "primary", "primary"];
+    var class_btn = ["primary", "success", "success", "primary", "success", "success", "primary", "success", "success", "primary", "success", "success"];
+    var texts = [
+        "取得したい資格",
+        "ひとりで生きていける？",
+        "高校生の時に流行っていた曲",
+        "もしも異性に２４時間なれたら何する？",
+        "これはとくに好き、っていう本や映画は？",
+        "火事のときに持って逃げるもの、3つ教えて！",
+        "地元の特産品は？",
+        "１ヶ月毎日食べるとしたら何？",
+        "宝くじで1億円があたったら何に使う？",
+        "今ハマっていること、おすすめなことは何？",
+        "今までの人生で会った人の中で「一番魅力的な人」は？",
+        "最近、笑ったことは？",
+    ];
+}else if(action == 4){
+    var head = ["内定者向け", "新卒向け", "社員向け", "内定者向け", "新卒向け", "内定者向け", "新卒向け", "社員向け", "内定者向け", "新卒向け"];
+    var class_bg = ["info", "success", "primary", "info", "success", "info", "success", "primary", "info", "success"];
+    var class_btn = ["primary", "info", "success", "primary", "info", "primary", "info", "success", "primary", "info"];
+    var texts = [
+        "外国人になるならどこの国？",
+        "明日地球が滅びるとしたら何がしたい？",
+        "ひとりで生きていける？",
+        "もしも異性に２４時間なれたら何する？",
+        "１ヶ月毎日食べるとしたら何？",
+        "最近、笑ったことは？",
+        "仕事で困ってること",
+        "火事のときに持って逃げるもの、3つ教えて！",
+        "今ハマっていること、おすすめなことは何？",
+        "宝くじで1億円があたったら何に使う？",
+    ];
+}else if(action == 5){
+    var head = ["内定者向け", "新卒向け", "社員向け", "内定者向け", "社員向け", "内定者向け", "新卒向け", "社員向け", "内定者向け", "社員向け"];
+    var class_bg = ["info", "success", "primary", "info", "primary", "info", "success", "primary", "info", "primary"];
+    var class_btn = ["primary", "info", "success", "primary", "success", "primary", "info", "success", "primary", "success"];
+    var texts = [
+        "取得したい資格",
+        "最近欲しい家電は",
+        "須田さんの第一印象は？",
+        "火事のときに持って逃げるもの、3つ教えて！",
+        "最近、笑ったことは？",
+        "もしも異性に２４時間なれたら何する？",
+        "明日地球が滅びるとしたら何がしたい？",
+        "宝くじで1億円があたったら何に使う？",
+        "学生時代のアルバイトとか部活は？",
+        "ひとりで生きていける？",
+    ];
+}else if(action == 6){
+    var head = ["内定者向け", "新卒向け", "社員向け", "社員向け", "内定者向け", "社員向け", "内定者向け", "新卒向け", "社員向け", "社員向け", "内定者向け", "社員向け"];
+    var class_bg = ["info", "success", "primary", "primary", "info", "primary", "info", "success", "primary", "primary", "info", "primary"];
+    var class_btn = ["primary", "info", "success", "success", "primary", "success", "primary", "info", "success", "success", "primary", "success"];
+    var texts = [
+        "最近、笑ったことは？",
+        "仕事で困ってること",
+        "最近感動したことや涙したことは何？",
+        "高校生の時に流行っていた曲",
+        "宝くじで1億円があたったら何に使う？",
+        "１ヶ月毎日食べるとしたら何？",
+        "これはとくに好き、っていう本や映画は？",
+        "明日地球が滅びるとしたら何がしたい？",
+        "ひとりで生きていける？",
+        "須田さんの第一印象は？",
+        "地元の特産品は？",
+        "火事のときに持って逃げるもの、3つ教えて！",
+    ];
+}
 var index = 0;
 function changeText(){
     document.getElementById("myBtn").value="次へ";
@@ -33,24 +114,21 @@ function changeText(){
     var myBtn = document.getElementById("myBtn");
     var text = document.getElementById("myText");
     var display = document.getElementById("display");
-    if(index == texts.length)index = 0;
-    if(index % 3 == 0){
-        display.innerHTML="内定者向け";
-        classChange1.className = "card-header h-75 bg-info";
-        classChange2.className = "card-header h-75 bg-info";
-        myBtn.className = "btn btn-primary text-center wi-150 mt-1";
-    }else if(index % 3 == 1){
-        display.innerHTML="新卒向け";
-        classChange1.className = "card-header h-75 bg-success";
-        classChange2.className = "card-header h-75 bg-success";
-        myBtn.className = "btn btn-info text-center wi-150 mt-1";
-    }else{
-        display.innerHTML="社員向け";
-        classChange1.className = "card-header h-75 bg-primary";
-        classChange2.className = "card-header h-75 bg-primary";
-        myBtn.className = "btn btn-success text-center wi-150 mt-1";
+    if(index == texts.length){
+        location.href='end.html';
     }
+    display.innerHTML = head[index];
+    classChange1.className = "card-header h-75 bg-" + class_bg[index];
+    classChange2.className = "card-header h-75 bg-" + class_bg[index];
+    myBtn.className = "btn btn-" + class_btn[index] + " text-center wi-150 mt-1";
     text.innerHTML = texts[index];
     index++;
     return;
+}
+
+function getUrlParameterAndSendToUrl(){
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+    var action = url.searchParams.get("action");
+    location.href='question.html?action=' + action;
 }
